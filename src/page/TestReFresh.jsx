66e5601bf96d9f8c6638c7e45b54ae661507ded3
zhongmeizhi <330447171@qq.com>
2019-11-05 15:41:52 +0800

    test

--- src/page/TestReFresh.jsx
+++ src/page/TestReFresh.jsx
@@ -4,14 +4,31 @@ import ReFresh from '@/utils/ReFresh.jsx'
 
 class TestReFresh extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      txt: '下拉刷新',
+      idx: 0
+    }
+  }
+
   freshHandler = () => {
     console.log('刷新一下');
+    this.setState((preState) => {
+      console.log(preState, 'preState')
+      return {
+        txt: '下拉刷新执行完成',
+        idx: preState.idx + 1
+      }
+    })
   }
 
   render() {
+    const testTxt = `${this.state.txt}刷新次数${this.state.idx}`;
+
     return <div className="test-reFresh">
       <ReFresh freshHandler={this.freshHandler}>
-        <div className="test-content">这个是下拉刷新内容</div>
+        <div className="test-content">{testTxt}</div>
       </ReFresh>
     </div>
   }
