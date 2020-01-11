86c57b1b37dba25ad2e5b4c976b139fff23fbfa0
zhongmeizhi <330447171@qq.com>
2020-01-11 19:10:11 +0800

    bisheng设置

---
+++ site/theme/template/AppShell.jsx
@@ -0,0 +1,9 @@
+/**
+ * Empty component for app shell
+ * See https://github.com/NekR/offline-plugin/blob/master/docs/app-shell.md
+ */
+import React from 'react';
+
+const AppShell = () => <div />;
+
+export default AppShell;
