5a38d55eda3ad1832f006645a2c2c9b390c14bc9
zhongmeizhi <330447171@qq.com>
2020-02-20 20:54:48 +0800

    Notice可以使用

--- src/views/Notice.tsx
+++ src/views/Notice.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 import { ReactComponent as NoticeSvg } from '../static/notice.svg'
 
 interface NoticeProps {
-    text: string
+    text: string,
+    scroll: boolean
 }
 
-export default function Notice({text} : NoticeProps) {
+export default function Notice({text, scroll = true} : NoticeProps) {
+
+    const [translateX, setTranslateX] = useState(0);
+    const [refWidth, setWidth] = useState();
+    const refItem = useRef(null);
+
+    let intervalHandler: number;
+
+    function step() {
+        if (refWidth === -translateX) {
+            setTranslateX(0);
+        } else {
+            setTranslateX(translateX - 1);
+        }
+        intervalHandler = window.requestAnimationFrame(step);
+        // intervalHandler = setTimeout(step, 120);
+    }
+      
+    useEffect(() => {
+        if (scroll) {
+            setWidth((refItem.current as any).offsetWidth);
+
+            intervalHandler = window.requestAnimationFrame(step);
+            // setTimeout(step, 120);
+        }
+        return () => {
+            intervalHandler && window.cancelAnimationFrame(intervalHandler)
+        };
+    }, [translateX])
+
     return <div className="zui-notice">
         <div className="zui-notice-icon">
             <NoticeSvg
@@ -16,8 +46,13 @@ export default function Notice({text} : NoticeProps) {
             ></NoticeSvg>
         </div>
         <div className="zui-notice-box">
-            <div className="zui-notice-item">
-                {text}
+            <div style={{transform: `translateX(${translateX}px)`}}>
+                <div className="zui-notice-item" ref={refItem}>
+                    {text}
+                </div>
+                <div className="zui-notice-item">
+                    {text}
+                </div>
             </div>
         </div>
     </div>
